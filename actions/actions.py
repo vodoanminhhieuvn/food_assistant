ffb2f33d82874263c124ae9f76634b7838bea6b0
hieuvo <hieuvo@lecle.co.kr>
2021-10-25 00:32:36 +0700

    feat[food_recipe]: Get API from Edamam

---
+++ actions/actions.py
@@ -0,0 +1,71 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+import rasa_sdk
+from rasa_sdk.types import DomainDict
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import (
+    SlotSet,
+    UserUtteranceReverted,
+    ConversationPaused,
+    EventType,
+)
+
+import os
+
+from actions.api.food_api import FoodAPI
+
+
+class ActionSearchFoodRecipe(Action):
+    def name(self) -> Text:
+        return "action_search_food_recipe"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        print("Hello World")
+        entities = [
+            e["value"]
+            for e in tracker.latest_message["entities"]
+            if e["entity"] == "ingredient"
+        ]
+
+        if not entities:
+            dispatcher.utter_message(
+                text="Im not sure what kind recipe you want, can you repeat ?"
+            )
+        else:
+            dispatcher.utter_message(text=f"Let me get {entities[0]} for you, ")
+            dispatcher.utter_message(text="Wait for minutes, ")
+
+            return [
+                SlotSet("ingredient", entities[0]),
+                rasa_sdk.events.FollowupAction("action_get_food_recipe"),
+            ]
+
+
+class ActionGetFoodRecipe(Action):
+    def name(self) -> Text:
+        return "action_get_food_recipe"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        print("Hello World")
+        ingredients = tracker.get_slot("ingredient")
+
+        print(ingredients)
+
+        dispatcher.utter_message(text=f"You want {ingredients} ?")
+
+        # food_label = FoodAPI.get_food_recipe(ingredients=ingredients)
+        # dispatcher.utter_message(text=f"{food_label}")
+
+        return []
